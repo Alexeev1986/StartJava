38b1d6953347f154b5e047f6e8ddd3167bd10e9b
Anton Alexeev <gonza_ant@mail.ru>
2025-02-24 09:50:00 +1000

    Исправил ошибки в Calculator и GuessNumber

--- Lesson 1/Calculator.java
+++ Lesson 1/Calculator.java
@@ -4,32 +4,28 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
         System.out.println("Введите 1 число ");
-        int variable1 = console.nextInt();
+        int number1 = console.nextInt();
         System.out.println("Введите мат действие (+, -, *, /, %, ^) ");
         char matAction = console.next().charAt(0);
         System.out.println("Введите 2 число ");
-        int variable2 = console.nextInt();
+        int number2 = console.nextInt();
         
-        int answer = 0;
+        int result = 0;
 
         if (matAction == '+') {
-            answer = variable1 + variable2;
+            result = number1 + number2;
         } else if (matAction == '-') {
-            answer = variable1 - variable2;
+            result = number1 - number2;
         } else if (matAction == '/') {
-            answer = variable1 / variable2;
+            result = number1 / number2;
         } else if (matAction == '%') {
-            answer = variable1 % variable2;
+            result = number1 % number2;
         } else if (matAction == '*') {
-            answer = variable1 * variable2;
+            result = number1 * number2;
         } else if (matAction == '^') {
-            int counter = variable2;
-            answer = variable1;
-            while (counter > 1) {
-                answer *= variable1;
-                counter--;
-            }
+            result = number1;
+            for(int i = 1; i < number2; i++) result *= number1;
         }
-        System.out.println(variable1 + " " + matAction + " " + variable2 + " = " + answer);
+        System.out.println(number1 + " " + matAction + " " + number2 + " = " + result);
     }
 }
