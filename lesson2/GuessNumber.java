ab1b77226f883b81d6d26c8388d6356de9bd5f46
Anton Alexeev <gonza_ant@mail.ru>
2025-09-07 08:19:06 +0300

    исправил указанные ошибки

--- lesson2/GuessNumber.java
+++ lesson2/GuessNumber.java
@@ -16,32 +16,29 @@ public class GuessNumber {
         Scanner console = new Scanner(System.in);
         Random random = new Random();
         int targetNumber = random.nextInt(1, 10);
-        boolean gameIsRunning = true;
-        while (gameIsRunning) {
+        while (true) {
             System.out.print("\n" + player1.getName() + " - угадывает какое число загадал компьютер? ");
             player1.setGuess(console.nextInt());
-            boolean isWinPlayer1 = isGuessed(player1.getGuess(), targetNumber);
+            boolean isWinPlayer1 = isGuessed(player1, targetNumber);
             if (isWinPlayer1) {
-                System.out.println("\n" + player1.getName() + " победил!");
-                gameIsRunning = false;
-                continue;
+                break;
             }
             System.out.print("\n" + player2.getName() + " - угадывает какое число загадал компьютер? ");
             player2.setGuess(console.nextInt());
-            boolean isWinPlayer2 = isGuessed(player2.getGuess(), targetNumber);
+            boolean isWinPlayer2 = isGuessed(player2, targetNumber);
             if (isWinPlayer2) {
-                System.out.println("\n" + player2.getName() + " победил!");
-                gameIsRunning = false;
+                break;
             }
         }
     }
 
-    public boolean isGuessed(int playerNumber, int targetNumber) {
-        if (playerNumber != targetNumber) {
-            System.out.print("\n" + playerNumber +
-                    (playerNumber > targetNumber ? " больше " : " меньше ") + "того, что загадал компьютер");
+    public boolean isGuessed(Player player, int targetNumber) {
+        if (player.getGuess() != targetNumber) {
+            System.out.print("\n" + player.getGuess() +
+                    (player.getGuess() > targetNumber ? " больше " : " меньше ") + "того, что загадал компьютер");
             return false;
         }
+        System.out.println("\n" + player.getName() + " победил!");
         return true;
     }
 }
