376f2c771602b2793423743df1b50d488efecfbf
Anton Alexeev <gonza_ant@mail.ru>
2025-10-16 09:55:57 +0300

    Исправил ошибки в: init_db.sql, populate.sql, queries.sql

--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -16,20 +16,23 @@ ORDER BY model_name;
 ORDER BY model_name;
 
 \echo '4. Все роботы, кроме Mark-1 и Mark-4 (по mark DESC)'
-  SELECT * FROM jaegers 
+  SELECT * 
+    FROM jaegers 
    WHERE mark NOT IN ('Mark-1', 'Mark-4') 
 ORDER BY mark DESC;
 
 \echo '5. Самые старые роботы (по дате запуска)'
   SELECT * 
     FROM jaegers 
-   WHERE launch = (SELECT MIN(launch) FROM jaegers)
+   WHERE launch = (SELECT MIN(launch)
+                     FROM jaegers)
 ORDER BY model_name;
 
 \echo '6. Роботы с максимальным количеством убитых кайдзю '
   SELECT model_name, mark, launch, kaiju_kill 
     FROM jaegers 
-   WHERE kaiju_kill = (SELECT MAX(kaiju_kill) FROM jaegers)
+   WHERE kaiju_kill = (SELECT MAX(kaiju_kill)
+                         FROM jaegers)
 ORDER BY model_name;
 
 \echo '7. Средний вес роботов (округлён до 3 знаков) '
@@ -40,10 +43,14 @@ ORDER BY model_name;
   UPDATE jaegers 
      SET kaiju_kill = kaiju_kill + 1 
    WHERE status != 'Destroyed';
-  SELECT * FROM jaegers ORDER BY model_name;
+  SELECT * 
+    FROM jaegers 
+ORDER BY model_name;
 
 \echo ' 9. Удаление уничтоженных роботов и вывод оставшихся '
-DELETE 
-  FROM jaegers 
- WHERE status = 'Destroyed';
-SELECT * FROM jaegers ORDER BY model_name;
+  DELETE 
+    FROM jaegers 
+   WHERE status = 'Destroyed';
+  SELECT * 
+    FROM jaegers 
+ORDER BY model_name;
